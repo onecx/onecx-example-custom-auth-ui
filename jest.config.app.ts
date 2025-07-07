5d479d591967710c3533c2d138a645ae692ce38f
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-07-07 07:59:07 +0200

    feat: initial impl

---
+++ jest.config.app.ts
@@ -0,0 +1,25 @@
+/* eslint-disable */
+export default {
+  displayName: 'onecx-custom-auth',
+  preset: './jest.preset.js',
+  coverageDirectory: './coverage/onecx-custom-auth',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
